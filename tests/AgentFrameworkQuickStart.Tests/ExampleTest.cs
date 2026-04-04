40ebaaf74c5392d3663e96625ce5ec65f47fa306
Tural Hasanov <hasanovtural11@gmail.com>
2026-04-04 13:49:24 +0200

    chore: Remove push triggers from Checkov and tfsec workflows; add ExampleTest unit test

---
+++ tests/AgentFrameworkQuickStart.Tests/ExampleTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AgentFrameworkQuickStart.Tests;
+
+[Trait("Category", "Unit")]
+public class ExampleTest
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
